d286141b69c9af38205459a0ee7018ee8241835e
Shrey Birmiwal <shreybirmiwal@gmail.com>
2025-12-05 14:34:45 -0500

    Fix proxy target to point to backend (127.0.0.1:5000) instead of frontend

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,21 +1,12 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function (app) {
-  console.log('[setupProxy] Configuring proxy middleware...');
-  
-  const proxyMiddleware = createProxyMiddleware({
-    target: 'http://127.0.0.1:5000',
-    changeOrigin: true,
-    secure: false,
-    logLevel: 'debug',
-    onProxyReq: (proxyReq, req, res) => {
-      console.log(`[PROXY] ${req.method} ${req.url} -> http://127.0.0.1:5000${req.url}`);
-    },
-    onError: (err, req, res) => {
-      console.error('[PROXY ERROR]', err);
-    }
-  });
-  
-  app.use('/admin', proxyMiddleware);
-  console.log('[setupProxy] Proxy configured for /admin routes');
+module.exports = function(app) {
+  // Proxy all /admin requests to the backend server
+  app.use(
+    '/admin',
+    createProxyMiddleware({
+      target: 'http://127.0.0.1:5000',
+      changeOrigin: true,
+    })
+  );
 };
