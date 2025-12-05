5ee7d06530f253c7fc157e39c790aeba19606c67
Shrey Birmiwal <shreybirmiwal@gmail.com>
2025-12-05 14:18:11 -0500

    Update proxy configuration with websocket support

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,12 +1,13 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Proxy all /admin API routes to the backend
   app.use(
     '/admin',
     createProxyMiddleware({
       target: 'http://127.0.0.1:5000',
       changeOrigin: true,
+      ws: true, // Enable websocket proxying
     })
   );
 };
